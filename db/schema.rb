5361ce020e828062d3df2cec2a998064d39ad28f
thkocks <timhkocks@gmail.com>
2017-05-30 18:40:09 +0200

    Start main page categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170530130650) do
 
   # These are extensions that must be enabled in order to support this database
@@ -129,7 +128,6 @@ ActiveRecord::Schema.define(version: 20170530130650) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
