5205c523f04b2f51f7eaf7344e0bbe621f4fd0f6
jobvanduuren <job_van_duuren@hotmail.com>
2017-05-30 18:27:48 +0200

    created a show and index for products and updated the styling sheet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product1 = Product.create(name: "Fender", price: 200, stock: 10, description: "Nice guitar", image_url: "http://www.takamine.com/templates/default/images/gclassical.png" )
+product2 = Product.create(name: "Les Paul", price: 300, stock: 11, description: "Really nice guitar", image_url: "http://images.epiphone.com.s3.amazonaws.com/Products/Les-Paul/Les-Paul-Standard-Plustop-PRO/Gallery/POP_LPSTDPLUSPRO-HB.jpg" )
