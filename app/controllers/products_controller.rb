27f8985e4ee701746dcecd7287e9756e1b90fee7
Hansterdam <hankortekaas@gmail.com>
2017-06-01 18:12:09 +0200

    Add delete button to the cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_order
+  before_action :set_order_item, only: %i[show index]
 
   def show
     @product = Product.find(params[:id])
@@ -7,6 +8,11 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+  end
+
+  private
+
+  def set_order_item
     @order_item = @order.order_items.new
   end
 end
